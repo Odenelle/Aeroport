60a5ed3ca10e8966ba8492e9f73020818542d768
Odenelle <pthieb@hotmail.fr>
2016-01-04 18:50:01 +0100

    ajout des packages

---
+++ src/projet/aeroport/ProjetAeroport.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projet.aeroport;
+
+/**
+ *
+ * @author Pierre
+ */
+public class ProjetAeroport {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("gogogo");
+        
+        /*
+        Bonjour,
+pour l’ArrayList, elle traduit en général une relation d’agrégation 
+        (donc non bidirectionnelle) qui est complètement différente de la 
+        relation d’héritage (non bidirectionnelle également), du coup il faut la
+        préciser bien sûr …
+pour Calendar, c’est un peu plus compliqué mais c’est tout à fait faisable et 
+        intéressant … 
+Ok pour pilote/copilote
+Les Hashmap ne sont pas obligatoires … 
+De plus vous devez pouvoir vous passer des classes retard et statut … 
+        */
+                
+    }
+    
+}
